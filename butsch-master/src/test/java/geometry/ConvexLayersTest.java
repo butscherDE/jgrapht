4cc8c30f35ba49a71461785eaab6c2739b80839c
Daniel Metzger <daniel.metzger@uni-konstanz.de>
2020-03-19 13:35:42 +0100

    testing convex layers with duplicates in input

--- butsch-master/src/test/java/geometry/ConvexLayersTest.java
+++ butsch-master/src/test/java/geometry/ConvexLayersTest.java
@@ -2,18 +2,15 @@ package geometry;
 
 import org.junit.jupiter.api.Test;
 import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.Coordinates;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.geom.GeometryFactory;
-
-import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class ConvexLayersTest {
     @Test
     public void testNestedSquares() {
-        final ConvexLayers cl = getConvexLayers();
+        final ConvexLayers cl = getConvexLayersNoDuplicates();
 
         final Geometry outerLayer = cl.layers[0];
         final Geometry innerLayer = cl.layers[1];
@@ -26,7 +23,7 @@ public class ConvexLayersTest {
         assertArrayEquals(expectedInnerCoordinates, innerCoordinates);
     }
 
-    private ConvexLayers getConvexLayers() {
+    private ConvexLayers getConvexLayersNoDuplicates() {
         final Coordinate[] coordinates = new Coordinate[] {new Coordinate(-1, -1),
                                                            new Coordinate(-1, 1),
                                                            new Coordinate(1,1),
@@ -56,5 +53,34 @@ public class ConvexLayersTest {
                                                                         new Coordinate(2, -2),
                                                                         new Coordinate(-2,-2)};
         return expectedInnerCoordinates;
+    }
+
+    @Test
+    public void testNestedSquaresDuplicates() {
+        final ConvexLayers cl = getConvexLayersDuplicateEndPoints();
+
+        final Geometry outerLayer = cl.layers[0];
+        final Geometry innerLayer = cl.layers[1];
+        final Coordinate[] outerCoordinates = outerLayer.getCoordinates();
+        final Coordinate[] innerCoordinates = innerLayer.getCoordinates();
+
+        final Coordinate[] expectedOuterCoordinates = getExpectedOuterCoordinates();
+        final Coordinate[] expectedInnerCoordinates = getExpectedInnerCoordinates();
+        assertArrayEquals(expectedOuterCoordinates, outerCoordinates);
+        assertArrayEquals(expectedInnerCoordinates, innerCoordinates);
+    }
+
+    private ConvexLayers getConvexLayersDuplicateEndPoints() {
+        final Coordinate[] coordinates = new Coordinate[] {new Coordinate(-1, -1),
+                                                           new Coordinate(-1, 1),
+                                                           new Coordinate(1,1),
+                                                           new Coordinate(1, -1),
+                                                           new Coordinate(-2,-2),
+                                                           new Coordinate(-2, 2),
+                                                           new Coordinate(2, 2),
+                                                           new Coordinate(2, -2)};
+
+        final Geometry geometry = new GeometryFactory().createMultiPointFromCoords(coordinates);
+        return new ConvexLayers(geometry);
     }
 }
