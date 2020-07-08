0ee6c07ea3cfefc57b0b5b6a25381a1533c02879
Daniel Metzger <daniel.metzger@uni-konstanz.de>
2020-07-08 17:24:26 +0200

    Implemented LOTNodeExtractionSpeed

--- butsch-master/src/main/java/evalutation/lotCalc/LOTNodeExtractionSpeed.java
+++ butsch-master/src/main/java/evalutation/lotCalc/LOTNodeExtractionSpeed.java
@@ -2,76 +2,114 @@ package evalutation.lotCalc;
 
 import data.*;
 import evalutation.Config;
+import evalutation.DataInstance;
+import evalutation.TestRegion;
+import evalutation.TestRegionCreator;
+import index.GridIndex;
 import org.jgrapht.alg.util.Pair;
-import org.locationtech.jts.geom.Polygon;
 import routing.DijkstraCHFactory;
+import routing.RPHAST;
 import routing.RPHASTFactory;
+import routing.RoutingAlgorithm;
+import routing.regionAware.util.EntryExitPointExtractor;
 import storage.ImportPBF;
 
 import java.io.FileNotFoundException;
 import java.time.LocalDateTime;
 import java.util.*;
-import java.util.stream.LongStream;
-import java.util.stream.Stream;
 
 public class LOTNodeExtractionSpeed {
     private final static String PBF = Config.PBF_ANDORRA;
-    private final static long numSources = 10;
-    private final static long numTargets = 10;
-    private final static long numPolygonsEachSource = 100;
+    private final static int numSourceTarget = 10;
+    private final static int numPolygonsEachSource = 100;
     private final static Random random = new Random(1337);
 
-    private final static String[] DUMP_HEADER = new String[]{"id", "relationId", "naiveTime", "rphastTime"};
+    private final static String[] DUMP_HEADER = new String[]{"relationId", "numEENodes", "naiveTime", "rphastTime"};
     private final static String RESULT_PATH = Config.LOT + LocalDateTime.now().toString().replaceAll(":", "_") + ".csv";
     private final static char DELIMITER = ',';
 
     private static DijkstraCHFactory dijkstraCHFactory;
     private static RPHASTFactory rphastFactory;
 
-    private static RoadGraph graph;
+    private static DataInstance instance;
     private static RoadCH roadCh;
-    private static List<Polygon> polygons;
+    private static List<TestRegion> testRegions;
     private static List<Node> vertices;
     private static Set<Node> sources;
     private static Set<Node> targets;
 
+    private static List<List<Object>> results = new ArrayList<>(DUMP_HEADER.length);
 
     public static void main(String[] args) {
+        init();
 
+        for (final TestRegion testRegion : testRegions) {
+            sampleSourceTarget();
+            final Set<Node> entryExitPoints = createEntryExitPoints(testRegion);
+            measure(testRegion, entryExitPoints);
+        }
+    }
+
+    public static void measure(final TestRegion testRegion, final Set<Node> entryExitPoints) {
+        final long durationNaive = getDurationNaive(entryExitPoints);
+        final long durationRphast = getDurationRphast(entryExitPoints);
+        addResult(testRegion, entryExitPoints, durationNaive, durationRphast);
+    }
+
+    public static long getDurationNaive(final Set<Node> entryExitPoints) {
+        final long naiveNanos0 = System.nanoTime();
+        getAllPathsNaive(entryExitPoints);
+        final long naiveNanos1 = System.nanoTime();
+        return naiveNanos1 - naiveNanos0;
+    }
+
+    public static long getDurationRphast(final Set<Node> entryExitPoints) {
+        final long rphastNanos0 = System.nanoTime();
+        getAllPathsRphast(entryExitPoints);
+        final long rphastNanos1 = System.nanoTime();
+        return rphastNanos1 - rphastNanos0;
+    }
+
+    public static void addResult(final TestRegion testRegion, final Set<Node> entryExitPoints, final long durationNaive,
+                                 final long durationRphast) {
+        results.get(0).add(testRegion.id);
+        results.get(1).add(entryExitPoints.size());
+        results.get(2).add(durationNaive);
+        results.get(3).add(durationRphast);
     }
 
     private static void init() {
-        final ImportPBF importPBF = assignGraph();
+        assignInstance();
         assignCh();
+        assignTestRegions();
         createDijkstraFactory();
         createRphastFactory();
+
+        for (int i = 0; i < DUMP_HEADER.length; i++) {
+            results.add(new ArrayList<>(testRegions.size()));
+        }
     }
 
-    private static ImportPBF assignGraph() {
+    private static void assignInstance() {
         final ImportPBF importPBF = new ImportPBF(PBF);
         try {
-            graph = importPBF.createGraph();
+            final RoadGraph graph = importPBF.createGraph();
+            final List<NodeRelation> relations = importPBF.getNodeRelations();
+
+            instance = DataInstance.createFromData(graph, new GridIndex(graph, 25, 25), relations);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             throw new IllegalStateException();
         }
-        vertices = new ArrayList<>(graph.vertexSet());
-
-        return importPBF;
+        vertices = new ArrayList<>(instance.graph.vertexSet());
     }
 
     private static void assignCh() {
-        roadCh = new CHPreprocessing(graph).createCHGraph();
+        roadCh = new CHPreprocessing(instance.graph).createCHGraph();
     }
 
-    private static void assignPolygons(final ImportPBF importPBF) {
-        final List<NodeRelation> nodeRelations = importPBF.getNodeRelations();
-        final int numNodeRelations = nodeRelations.size();
-        final Stream<NodeRelation> realPolygons = LongStream
-                .rangeClosed(1, numPolygonsEachSource)
-                .mapToObj(i -> nodeRelations.get(random.nextInt(numNodeRelations)));
-
-//        Impor
+    private static void assignTestRegions() {
+        testRegions = new TestRegionCreator(instance, numPolygonsEachSource, polygon -> true).getTestEntities();
     }
 
     private static void createDijkstraFactory() {
@@ -82,20 +120,39 @@ public class LOTNodeExtractionSpeed {
         rphastFactory = new RPHASTFactory(roadCh, false);
     }
 
-    private static void sampleSourceNodes() {
-        for (long i = 0; i < numSources; i++) {
-            sources.add(vertices.get(random.nextInt(vertices.size())));
-        }
+    private static Set<Node> createEntryExitPoints(final TestRegion testRegion) {
+        final RegionOfInterest roi = new RegionOfInterest(testRegion.polygon);
+        final EntryExitPointExtractor eeExtractor = new EntryExitPointExtractor(roi, instance.index);
+        return eeExtractor.extract();
     }
 
-    private static void sampleTargetNodes() {
-        for (long i = 0; i < numTargets; i++) {
-            targets.add(vertices.get(random.nextInt(vertices.size())));
-        }
+    private static void sampleSourceTarget() {
+        sampleNodes(sources);
+        sampleNodes(targets);
+    }
+
+    private static void sampleNodes(final Set<Node> nodes) {
+        Collections.shuffle(vertices);
+        nodes.clear();
+        nodes.addAll(vertices.subList(0, Math.min(numSourceTarget, vertices.size())));
     }
 
     private static Map<Pair<Node, Node>, Path> getAllPathsNaive(final Set<Node> entryExitNodes) {
-    return null;
+        final Map<Pair<Node, Node>, Path> allPaths = new HashMap<>();
+        final RoutingAlgorithm routingAlgorithm = dijkstraCHFactory.createRoutingAlgorithm();
+
+        putAllCrossProductPaths(routingAlgorithm, allPaths, sources, entryExitNodes);
+        putAllCrossProductPaths(routingAlgorithm, allPaths, entryExitNodes, targets);
+
+        return allPaths;
+    }
+
+    private static void putAllCrossProductPaths(final RoutingAlgorithm routingAlgorithm, final Map<Pair<Node, Node>, Path> allPaths, final Set<Node> sources, final Set<Node> targets) {
+        for (final Node source : sources) {
+            for (final Node target : targets) {
+                allPaths.put(new Pair(source, target), routingAlgorithm.findPath(source, target));
+            }
+        }
     }
 
     private static Map<Pair<Node, Node>, Path> getAllPathsRphast(final Set<Node> entryExitNodes) {
@@ -105,7 +162,7 @@ public class LOTNodeExtractionSpeed {
         final Set<Node> rphastTargets = new HashSet<>(entryExitNodes);
         rphastTargets.addAll(targets);
 
-//        new RPHAST()
-        return null;
+        final RPHAST rphast = (RPHAST) rphastFactory.createRoutingAlgorithm();
+        return rphast.findPathsAsMap(rphastSources, rphastTargets);
     }
 }
